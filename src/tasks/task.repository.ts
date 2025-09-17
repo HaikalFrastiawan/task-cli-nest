a22e033614b525898a693e46677d01e8faf19322
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-17 12:59:49 +0700

    4.4 implementasi service dan repository melalui controller

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,10 +1,11 @@
-import {  readFile, writeFile } from "fs";
+import { readFile, writeFile } from "fs/promises";
 
 export class TaskRepository {
-    async findAll(): Promise<any> {
+    async findAll(): Promise<any[]> {
         const data = await readFile('tasks.json', 'utf-8');
-        return JSON.parse(data)
+        return JSON.parse(data);
     }
+
     async findOne(id: number) {
         const data = await readFile('tasks.json', 'utf-8');
         const tasks = JSON.parse(data);
@@ -12,18 +13,19 @@ export class TaskRepository {
         return tasks.find((task: any) => task.id === id);
     }
 
-    async create(task: string) {
+    async create(content: string) {
         const data = await readFile('tasks.json', 'utf-8');
         const tasks = JSON.parse(data);
 
         const newTask = {
-            id: tasks[task.length - 1].id + 1,
-            content: task,
+            id: tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1,
+            content,
         };
 
         tasks.push(newTask);
 
-        await writeFile('tasks.json', JSON.stringify(tasks, null, 2),'utf-8');
+        await writeFile('tasks.json', JSON.stringify(tasks, null, 2), 'utf-8');
 
+        return newTask; // 
     }
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,21 +1,29 @@
 import { Controller, Get, Post, Body,Param } from '@nestjs/common';
 import { CreateTaskDto } from './dtos/createTask.dto';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+
+    taskService: TasksService;
+
+    constructor(){
+        this.taskService = new TasksService()
+    }
+
     @Get()
     listTask(){
-        return 'this all task home';
+        return this.taskService.findAll();
     }
 
     @Post()
     createTask(@Body() body: CreateTaskDto) {
-        return body;
+        return this.taskService.create(body.content);
     }
 
     @Get('/:id')
     getTask(@Param('id') id: string) {
-        return `this task by id : ${id}`
+        return this.taskService.findOne(parseInt(id));
     }
 
     
