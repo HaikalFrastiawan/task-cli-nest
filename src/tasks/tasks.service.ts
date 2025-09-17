09f3532bd31f93cb492027615061e85c40959d06
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-17 12:29:20 +0700

    4.3 Service untuk Repository

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,21 @@
+import { TaskRepository } from "./task.repository";
+
+export class TasksService {
+    taskRepo: TaskRepository;
+    
+    constructor(){
+        this.taskRepo = new TaskRepository();
+    }
+
+    findAll(){
+        return this.taskRepo.findAll();
+    }
+
+    findOne(id: number){
+        return this.taskRepo.findOne(id);
+    }
+
+    create (task: string){
+        return this.taskRepo.create(task)
+    }
+}
